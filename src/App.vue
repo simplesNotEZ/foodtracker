f5978700afba186e0bd6443b32a7205e441f53a9
Benjamin Austin <benjamin.austin@c02qv9byg8wp.servicemagic.com>
2019-02-19 11:21:52 -0700

    is this the initial commit?

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <Header />
+    <Body />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import Body from '@/components/Body.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Body,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <img id="spaghetOWeiner" src="@/assets/spaghetOWeiner.jpg" />
+        <h1 id="real-header">9errrr Food Reviews</h1>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    name: 'Header',
+
+}
+
+</script>
+
+<style>
+
+#spaghetOWeiner {
+    height: 30vh;
+    width:  30wv;
+}
+
+#real-header {
+    color: green;
+}
+
+
+</style>
