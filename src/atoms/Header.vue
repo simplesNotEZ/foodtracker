f745cf9ce6ebbc6a1c1e8036f82f1b40fb8f123a
benjamin austin <benjamin.austin@homeadvisor.com>
2019-07-04 15:23:12 -0600

    Inital Commit.  Preliminary set-up of foodtracker app.

---
+++ src/atoms/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img id="spaghetOWeiner" src="@/assets/spaghetOWeiner.jpg">
+    <h1 id="real-header">HUB Spots Food Reviews</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style>
+#spaghetOWeiner {
+  height: 30vh;
+  width: 30wv;
+}
+
+#real-header {
+  color: green;
+}
+</style>
