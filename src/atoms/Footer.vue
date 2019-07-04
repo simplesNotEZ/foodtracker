ffb1e8d299479df5d29568d20c880f071c92d4bb
benjamin austin <benjamin.austin@homeadvisor.com>
2019-07-04 15:23:12 -0600

    Initial Commit.  Preliminary set-up of foodtracker app.

---
+++ src/atoms/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="footer-outer-div">
+        <div id="footer-inner-div">
+            <h2>Look at these shoes!</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    
+    name: 'Footer',
+    
+    data() {
+        return {
+
+        }
+    }
+}
+
+</script>
+
+
+<style>
+
+#footer-outer-div {
+    background-color: green;
+    display: flex;
+    flex-direction: column;
+}
+#footer-inner-div {
+    flex-shrink: 0;
+}
+
+h2 {
+    color: yellow;
+}
+
+</style>
---
+++ src/molecules/Body.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="body">
+        <p>Here's some content guay!</p>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+#body {
+    flex: 1;
+}
+
+</style>
