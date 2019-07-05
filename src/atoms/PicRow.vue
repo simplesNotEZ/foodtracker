fefdb21c68a995ccc192b9455f80a43c5d0ba2d0
benjamin austin <benjamin.austin@homeadvisor.com>
2019-07-05 08:22:48 -0600

    Here's the PicRow component

---
+++ src/atoms/PicRow.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img id="spaghetOWeiner" src="@/assets/spaghetOWeiner.jpg">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PicRow"
+};
+</script>
+
+<style scoped>
+#spaghetOWeiner {
+  height: 30vh;
+  width: 30vw;
+}
+</style>
